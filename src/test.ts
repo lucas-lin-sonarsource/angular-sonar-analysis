0ab7b93acf732e0dbab1bb8f67b825184543b8ee
Lucas Lin <lucas.lin@sonarsource.com>
2024-04-29 15:44:42 +0800

    new vulnerability issue sim

---
+++ src/test.ts
@@ -0,0 +1,9 @@
+const path = require('path');
+const fs   = require('fs');
+
+function (req, res) {
+  const targetDirectory = "/data/app/resources/";
+  const userFilename = path.join(targetDirectory, req.query.filename);
+
+  let data = fs.readFileSync(userFilename, { encoding: 'utf8', flag: 'r' }); // Noncompliant
+}
